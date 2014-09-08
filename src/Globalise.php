ac4b9123bd187baf7e9ff3459a69679c0d8d1470
Brad Jones <bj@gravit.com.au>
2014-09-08 15:40:02 +1000

    globalise assetmini automatically

---
+++ src/Globalise.php
@@ -0,0 +1,34 @@
+<?php
+////////////////////////////////////////////////////////////////////////////////
+//            _____                        __     _____   __        __          
+//           /  _  \   ______ ______ _____/  |_  /     \ |__| ____ |__|         
+//          /  /_\  \ /  ___//  ___// __ \   __\/  \ /  \|  |/    \|  |         
+//         /    |    \\___ \ \___ \\  ___/|  | /    Y    \  |   |  \  |         
+//         \____|__  /____  >____  >\___  >__| \____|__  /__|___|  /__|         
+//                 \/     \/     \/     \/             \/        \/             
+// -----------------------------------------------------------------------------
+//          Designed and Developed by Brad Jones <brad @="bjc.id.au" />         
+// -----------------------------------------------------------------------------
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Not sure if this is consider a big no no or not, I am still sitting on the
+ * fence about it. But basically all we are doing is checking to see if
+ * the class AssetMini doesn't already exist globally and that we are not being
+ * run inisde of a Laravel App (as we have the laravel service provider and
+ * facade to this job).
+ * 
+ * If both of those conditions are met then we alias the
+ * HtmlHelper class so that it makes things nice and concise.
+ * 
+ * So instead of doing this:
+ * 
+ *     use Gears\AssetMini\HtmlHelper as AssetMini;
+ * 
+ * You just get AssetMini automatically.
+ */
+
+if (!class_exists('\AssetMini') && !defined('LARAVEL_START'))
+{
+	class_alias('\Gears\AssetMini\HtmlHelper', '\AssetMini');
+}
